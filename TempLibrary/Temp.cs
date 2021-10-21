5be382aaeb292700081ec7c53f8eba3473df6105
sowmya-seetha <56246381+sowmya-seetha@users.noreply.github.com>
2021-10-21 19:08:11 +0530

    Assignment 3

---
+++ TempLibrary/Temp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TempLibrary
+{
+    public class Temp
+    {
+        public double CTF(double temp)
+        {
+            double farhient;
+            farhient = (temp * 9 / 5) + 32;
+            return farhient;
+        }
+        public double FTC(double temp)
+        {
+            double celsius;
+            
+            celsius = (temp - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
